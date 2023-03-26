0b23e26db5dc3a9c2247fa4fe60e739d8b474db4
Imane O <ouiyzmeimane@gmail.com>
2023-03-26 10:56:23 -0700

    added -printf

--- _printf.c
+++ _printf.c
@@ -1,12 +1,56 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdarg.h>
+#include "main.h"
 /**
- * _printf - custom printf
- * @format: character string
- * @...: arguments
- * Return: number of characters printed
-*/
+ * _printf - A function that prints a formatted string to stdout
+ * @format: The format string to be printed
+ * Return: The number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-/*your code*/
+    if (format == NULL)
+    {
+       return (-1);
+    }
+
+    int count = 0;
+    va_list args;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+
+            switch (*format)
+            {
+                case 'c':
+                    _putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                    count += printf("%s", va_arg(args, char *));
+                    break;
+                case '%':
+                    _putchar('%');
+                    count++;
+                    break;
+                default:
+                    /* unsupported conversion specifier, ignore it */
+                    break;
+            }
+        }
+        else
+        {
+            _putchar(*format);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return (count);
 }

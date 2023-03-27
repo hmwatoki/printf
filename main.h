11851e4023169f0f338a91a0298f63d984e726b0
hmwatoki <harrisonmwatoki@gmail.com>
2023-03-27 08:51:54 +0300

    Revert "feat: Added support functions for _printf()"
    This reverts commit 49d782fa3ed6840d3b6f46249e22a4eb626c15a0.

--- main.h
+++ main.h
@@ -9,9 +9,4 @@
 #include <stdio.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_number(int n);
-int print_unsigned_number(unsigned int n);
-int print_octal_number(unsigned int n);
-int print_hexadecimal_number(unsigned int n, char cs);
-int print_address(void *p);
 #endif

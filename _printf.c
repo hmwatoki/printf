3251de0bece4edbcd7569f73103cf8b4ac285a08
hmwatoki <harrisonmwatoki@gmail.com>
2023-03-27 16:32:30 +0300

    fix: Update _printf function to support 's', 'c', and '%' conversion specifiers

--- _printf.c
+++ _printf.c
@@ -1,79 +1,51 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
+#include <stdarg.h>
+
 /**
- * print_number - prints an integer using _putchar
- *
- * @n: the integer to print
- *
- * Return: void
- */
-void print_number(int n)
-{
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    if (n > 9) {
-        print_number(n / 10);
-    }
-    _putchar(n % 10 + '0');
-}
-/**
- * _printf - A function that prints a formatted string to stdout
- * @format: The format string to be printed
- * Return: The number of characters printed
+ * _printf - custom printf
+ * @format: character string
+ * @...: arguments
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    if (format == NULL)
-    {
-       return (-1);
-    }
-
-    int count = 0;
-    va_list args;
-
-    va_start(args, format);
-
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-
-            switch (*format)
-            {
-                case 'c':
-                    _putchar(va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    count += _puts(va_arg(args, char *));
-                    break;
-		case 'd':
-                case 'i':
-                    print_number(va_arg(args, int));
-                    count++;
-                    break;
-                case '%':
-                    _putchar('%');
-                    count++;
-                    break;
-                default:
-                    /* unsupported conversion specifier, ignore it */
-                    break;
-            }
-        }
-        else
-        {
-            _putchar(*format);
-            count++;
-        }
-        format++;
-    }
-
-    va_end(args);
-
-    return (count);
+va_list args;
+int count;
+va_start(args, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == 's')
+{
+const char *str = va_arg(args, const char *);
+while (*str != '\0')
+{
+putchar(*str);
+count++;
+str++;
+}
+}
+else if (*format == 'c')
+{
+char c = (char) va_arg(args, int);
+putchar(c);
+count++;
+}
+else if (*format == '%')
+{
+putchar('%');
+count++;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
 }

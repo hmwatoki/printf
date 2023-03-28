cb66b808e634505286bd3428550fc09b475f0dcc
Imane O <ouiyzmeimane@gmail.com>
2023-03-28 10:09:44 -0700

    updated hprint

--- testf/hprint.c
+++ testf/hprint.c
@@ -30,7 +30,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknow_len += write(1, "%%", 1);
+		unknow_len += write(1, "%", 1);
 		if (fmt[*ind - 1] == ' ')
 			unknow_len += write(1, " ", 1);
 		else if (width)
@@ -47,3 +47,4 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	}
 	return (printed_chars);
 }
+

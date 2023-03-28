89fe08765b5d54cf2d822b9152e7e2be08008048
Imane O <ouiyzmeimane@gmail.com>
2023-03-28 10:11:38 -0700

    restore hprint

--- testf/hprint.c
+++ testf/hprint.c
@@ -30,7 +30,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknow_len += write(1, "%", 1);
+		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
 			unknow_len += write(1, " ", 1);
 		else if (width)

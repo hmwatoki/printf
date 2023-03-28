3751713e199b6a9499e314dfb9504f101d69d8e7
hmwatoki <harrisonmwatoki@gmail.com>
2023-03-28 16:58:23 +0300

    Revert "Merge branch 'harry_branch'"
    This reverts commit 33098bf382d7e12dc17f4b2d4f375f0aa9adc156, reversing
    changes made to 88d285907c96cf0e1579ed0b71523523dc70cea1.

--- _printf.c
+++ _printf.c
@@ -1,32 +1,51 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 /**
- * _printf - implementation of the inbuilt printf
- * @format: the format specifier
- * Return: the formated string
+ * _printf - produces output according to a format
+ * @format: character string composed of zero or more directives
+ * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int printed = 0;
-
-	va_list args;
-
-	va_start(args, format);
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			printed = selector(format, args, printed);
-			format++;
-		}
-		else
-		{
-			_putchar(*format);
-			printed++;
-			format++;
-		}
-	}
-	va_end(args);
-	return (printed);
+va_list args;
+int count = 0;
+int i;
+va_start(args, format);
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] == '%')
+{
+i++;
+if (format[i] == 'c')
+{
+char c = (char)va_arg(args, int);
+count += _putchar(c);
+}
+else if (format[i] == 's')
+{
+char *s = va_arg(args, char *);
+int j;
+for (j = 0; s[j] != '\0'; j++)
+{
+count += _putchar(s[j]);
+}
+}
+else if (format[i] == '%')
+{
+count += _putchar('%');
+}
+else
+{
+count += _putchar('%');
+count += _putchar(format[i]);
+}
+}
+else
+{
+count += _putchar(format[i]);
+}
+}
+va_end(args);
+return (count);
 }
--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * _putchar - prints/writes character
- * @character: The character to print
- * Return: 1, success, -1 error
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 1 if succesful
+ * On error, -1 is returned, and errno is set.
  */
-
-int _putchar(char character)
+int _putchar(char c)
 {
-	return (write(1, &character, 1));
+return (write(1, &c, 1));
 }

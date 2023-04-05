fdef387a2422d7e509e3dffcc2201373e522ced2
hmwatoki <harrisonmwatoki@gmail.com>
2023-04-05 22:42:49 +0300

    Revert "feat: Added support functions for _printf()"
    This reverts commit 49d782fa3ed6840d3b6f46249e22a4eb626c15a0.

--- _printf.c
+++ _printf.c
@@ -6,27 +6,5 @@
  */
 int _printf(const char *format, ...)
 {
-	int printed = 0;
-
-	va_list args;
-
-	va_start(args, format);
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			printed = selector(format, args, printed);
-			format++;
-		}
-		else
-		{
-			_putchar(*format);
-			printed++;
-			format++;
-		}
-	}
-	va_end(args);
-	return (printed);
+/*your code*/
 }
--- main.h
+++ main.h
@@ -13,10 +13,4 @@ int printf_integer(va_list args, int printed);
 int selector(const char *format, va_list args, int printed);
 int printf_binary(unsigned int num, int printed);
 int _printf(const char *format, ...);
-int _x(unsigned int num, int printed, int uppercase);
-int printf_octal(unsigned int num, int printed);
-int printf_unsigned(unsigned int num, int printed);
-int printf_reverse(va_list args, int printed);
-int printf_pointer(va_list args, int printed);
-
 #endif
